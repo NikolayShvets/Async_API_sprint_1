7a332ce7155a0ac6d79278505adb2725029fad35
Швец Николай <kolya.shvets20@gmail.com>
2024-06-24 23:23:10 +0300

    ref: Убирает заглушку mypy

--- apps/movies/schemas/base.py
+++ apps/movies/schemas/base.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, ConfigDict
 
 
-class BaseSchema(BaseModel):  # type: ignore[no-redef]
+class BaseSchema(BaseModel):
     model_config = ConfigDict(from_attributes=True)
