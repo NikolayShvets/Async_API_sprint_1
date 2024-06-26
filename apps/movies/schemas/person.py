34a00696f55c3c3489772a35aede1747e78a9768
Швец Николай <kolya.shvets20@gmail.com>
2024-06-26 20:26:21 +0300

    fix: Правит модели персоналий

--- apps/movies/schemas/person.py
+++ apps/movies/schemas/person.py
@@ -1,10 +1,7 @@
 from uuid import UUID
-
-from models.person import PersonRole
 from schemas.base import BaseSchema
 
 
 class PersonSchema(BaseSchema):
     id: UUID
     full_name: str
-    role: PersonRole
