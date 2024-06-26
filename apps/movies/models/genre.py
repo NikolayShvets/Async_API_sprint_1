6b5831250d92d3d2f98c2fc38d8793b52b421e5d
nafedoseev <nafedoseev@domclick.ru>
2024-06-26 22:36:15 +0300

    fix genre model field type

--- apps/movies/models/genre.py
+++ apps/movies/models/genre.py
@@ -1,14 +1,16 @@
+from uuid import UUID
+
 from pydantic import BaseModel
 
 
 class GenreFilmwork(BaseModel):
-    id: str
+    id: UUID
     title: str
     imdb_rating: float | None
 
 
 class Genre(BaseModel):
-    id: str
+    id: UUID
     name: str
     description: str | None
     films: list[GenreFilmwork]
