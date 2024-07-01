7a521a06eb50449e641602c42785dcd38d2cdeac
a.v.cherkashin <a.v.cherkashin@tinkoff.ru>
2024-07-01 12:27:59 +0500

    _get_complex_id

--- apps/movies/services/base.py
+++ apps/movies/services/base.py
@@ -1,5 +1,6 @@
 import pickle
 from typing import TypeVar
+from uuid import UUID
 
 from models import Film, Genre, Person
 from redis.asyncio import Redis
@@ -13,19 +14,20 @@ class BaseService:
         self.cache_expire = 300
 
     @staticmethod
-    def _get_complex_id(method: str, item: ItemModel) -> str:
-        try:
-            item_id = item.id
-        except AttributeError:
-            item_id = None
+    def _get_complex_id(method: str, item: ItemModel | None = None, item_id: UUID | None = None) -> str:
+        if not item_id:
+            try:
+                item_id = item.id
+            except AttributeError:
+                item_id = None
 
         return f"{method}:{item_id}"
 
-    async def put_item_to_cache(self, method: str, item: ItemModel):
-        await self.redis.set(self._get_complex_id(method, item), pickle.dumps(item), self.cache_expire)
+    async def put_item_to_cache(self, method: str, item: ItemModel) -> None:
+        await self.redis.set(self._get_complex_id(method=method, item=item), pickle.dumps(item), self.cache_expire)
 
-    async def get_item_from_cache(self, method, item_id):
-        data = await self.redis.get(self._get_complex_id(method, item_id))
+    async def get_item_from_cache(self, method: str, item_id: UUID) -> ItemModel | None:
+        data = await self.redis.get(self._get_complex_id(method=method, item_id=item_id))
         if not data:
             return None
 
